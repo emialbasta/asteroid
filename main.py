f045acc4d29875d285e6a953e8a0a6a594f2cb7d
emialbasta <emialbaster2@hotmail.com>
2025-06-20 15:57:24 -0700

    asteroid and asteroid field completed

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import pygame
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import *
 def main():
 	pygame.init()
 	pyclock = pygame.time.Clock()
+	asteroid_group = pygame.sprite.Group()
+	asteroid_field_group = pygame.sprite.Group()
+	update_group = pygame.sprite.Group()
+	draw_group = pygame.sprite.Group()
+	AsteroidField.containers = (asteroid_field_group, update_group)
+	Asteroid.containers = (asteroid_group, update_group, draw_group)
+	Player.containers = (update_group, draw_group)
+	asteroid_field = AsteroidField()
 	dt = 0
 	x = SCREEN_WIDTH / 2
 	y = SCREEN_HEIGHT / 2
@@ -11,8 +21,9 @@ def main():
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	while True:
 		screen.fill("black")
-		player.update(dt)
-		player.draw(screen)
+		update_group.update(dt)
+		for object in draw_group:
+			object.draw(screen)
 		pygame.display.flip()
 		for event in pygame.event.get():
                 	if event.type == pygame.QUIT:
